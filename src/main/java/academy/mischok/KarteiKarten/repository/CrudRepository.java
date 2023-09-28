ea3c382cff342fe940cac6e7168ad6fe39b10987
Giordaninim <matteo.giordanini@gmail.com>
2023-09-28 19:42:16 +0200

    sm local change MG

--- src/main/java/academy/mischok/KarteiKarten/repository/CrudRepository.java
+++ src/main/java/academy/mischok/KarteiKarten/repository/CrudRepository.java
@@ -1,7 +1,7 @@
 package academy.mischok.KarteiKarten.repository;
 
 public interface CrudRepository<T, ID> {
-	Iterable<T> findAll();
+	Iterable<T> findAll(); //to find all files
 	T findById(ID id);
 	T save(T entity);
 	void delete(T entity);
